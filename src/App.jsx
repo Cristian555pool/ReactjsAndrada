d3bd25f37cb7a00097c380969b9ac450ecfbad08
Critian Andrada <cristianandrada286@gmail.com>
2022-12-12 23:55:24 -0300

    segunda preentrega

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import './App.css';
+import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
+import ItemDetailContainer from './Components/ItemDetailContainer/ItemDetailContainer';
+//import AvatarJsx from './components/AvatarJsx';
+//import Button from './components/Button/Button'; 
+import Counter from './Components/Counter/Counter';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+
+//  const accept = () => {
+//    console.log('cancele')
+//  }
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<ItemListContainer greeting={'Estos son nuestros productos disponibles!! ^^'}/>}/>
+          <Route path='/category/:categoryId' element={<ItemListContainer />} />
+          <Route path='/detail/:productId' element={<ItemDetailContainer />}/>            
+        </Routes>  
+      </BrowserRouter>
+      {/* <header className="App-header">*/}
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <AvatarJsx />
+        <Button label={'cancelar'} background='blue' action={accept}/>
+        <Button label={'aceptar'} background='green' action={() => console.log('acepte')}/> */}
+        {/* <h1 className='Titulo' style={{ backgroundColor: 'green'}} onClick={() => console.log('click')}>MisatoEve ^^</h1>      
+      <button>boton</button> 
+      <input /> */}       
+      {/* </header>*/}
+      <Counter /> 
+    </div>
+  );
+}
+
+export default App;
--- src/Components/CartWidget/CartWidget.js
+++ src/Components/CartWidget/CartWidget.js
@@ -2,10 +2,13 @@ import cart from './assets/cartr1.jpeg'
 
 const CartWidget = () => {
     return (
-        <div className="cart__count">
+        <div className='cart__count'> 
+            <div style={{ color: 'white' }}>
             <img src={cart} alt='cart'/>
-            3
+            3                 
+            </div>
         </div>
     )
 }
+
 export default CartWidget
